8f1820b44aeb060bf03dd91ded27169d9719ca79
Lisahtm <383015311@qq.com>
2015-11-08 16:13:32 +0800

    main 输出格式进行修改 文档修改

--- GSkyline/main.cpp
+++ GSkyline/main.cpp
@@ -12,7 +12,7 @@ int MyPrintf::cur_index = 0;
 char MyPrintf::print_str[buf_size];
 int main(int argc, char** argv)
 {
-	string filename = "data/corr_2.txt";
+	string filename = "data/corr_6.txt";
 	int k = 2;
 	//handle input by wx
 	printf("Usage: \n default mode:GSkyline \n Custom mode: GSkyline <data file> <gskyline size> <output: if empty use stdout\n");
@@ -37,30 +37,30 @@ int main(int argc, char** argv)
 	}
 
 	time_t start, end;
-	//cout << "building graph" << endl;
+	cout << "building graph" << endl;
 	start = GetTickCount();
 	GSkyline skyline(filename);
-	//cout << "size:" << skyline.allPoints.size() << endl;
-	//cout << "layers:" << skyline.layers.size() << endl;
+	cout << "size:" << skyline.allPoints.size() << endl;
+	cout << "layers:" << skyline.layers.size() << endl;
 	end = GetTickCount();
-	//printf("building cost time: %lld ms\n", end - start);
-	cout << filename << "\t";
-	cout << k << "\t";
-	printf("%lld\t", end - start);
+	printf("building cost time: %lld ms\n", end - start);
+	//cout << filename << "\t";
+	//cout << k << "\t";
+	//printf("%lld\t", end - start);
 
 	start = GetTickCount();
 	vector<Group> pre = skyline.preprocessing(k);
     end = GetTickCount();
-	//printf("processing cost time: %lld ms\n", end - start);
-	printf("%lld\t", end - start);
-	//cout << "preprocessing result:" << endl;
+	printf("processing cost time: %lld ms\n", end - start);
+	//printf("%lld\t", end - start);
+	cout << "preprocessing result:" << endl;
 	int preNum = pre.size();
 
 	/*for(vector<Group>::iterator it = pre.begin(); it != pre.end(); it++){
 		(*it).Print();
 	}*/
-	//cout << "preprocessing end" << endl;
-	//cout << "p wise:" << endl;
+	cout << "preprocessing end" << endl;
+	cout << "p wise:" << endl;
 	start = GetTickCount();
 	try{
 		skyline.PointWisePlus(k);
@@ -68,16 +68,16 @@ int main(int argc, char** argv)
 		(*it).Print();
 		}*/
 		end = GetTickCount();
-		//printf("get result group: %d\n", skyline.PointWiseCount + preNum);
-		printf("%d\t", skyline.PointWiseCount + preNum);
+		printf("get result group: %d\n", skyline.PointWiseCount + preNum);
+		//printf("%d\t", skyline.PointWiseCount + preNum);
 	}
 	catch (exception &e){
 		printf(e.what());
 	}
-	//cout << "pwise cost time: " << (end - start) << " ms" << endl;
-	cout <<(end - start) << "\t";
+	cout << "pwise cost time: " << (end - start) << " ms" << endl;
+	//cout <<(end - start) << "\t";
 
-	//cout << "u wise plus:" << endl;
+	cout << "u wise plus:" << endl;
 	start = GetTickCount();
 	/*vector<UGroup> gs1*/
 	
@@ -87,10 +87,10 @@ int main(int argc, char** argv)
 	//}
 	//printf("get result group: %d\n",gs1.size());
 	end = GetTickCount();
-	//cout << "u wise get result group num:" <<(uwiseNum+preNum) << endl;
-	//cout << "uwise cost time: "<<(end - start) <<" ms" << endl;
-	cout <<(uwiseNum + preNum) << "\t";
-	cout <<(end - start) << "\n";
+	cout << "u wise get result group num:" <<(uwiseNum+preNum) << endl;
+	cout << "uwise cost time: "<<(end - start) <<" ms" << endl;
+	//cout <<(uwiseNum + preNum) << "\t";
+	//cout <<(end - start) << "\n";
 
 	//printf("pwise cost time: %lld ms\n", end - start);
 	return 0;
