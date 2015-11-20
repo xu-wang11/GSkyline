4f0d465c1b8ca3464e6a2c5f3d223465694f09b9
softpoeter <kangrong2061992@163.com>
2015-11-20 19:35:13 +0800

    main test

--- GSkyline/main.cpp
+++ GSkyline/main.cpp
@@ -16,9 +16,9 @@ void testUwisePlusPlus2(){
 }
 int main(int argc, char** argv)
 {
-	string filename = "data/anti_4.txt";
-	//string filename = "data/test.txt";
-	int k = 3;
+	string filename = "data/anti_8.txt";
+	//string filename = "data/te3t.txt";
+	int k = 4;
 	//handle input by wx
 	printf("Usage: \n default mode:GSkyline \n Custom mode: GSkyline <data file> <gskyline size> <output: if empty use stdout\n");
 	if (argc == 1) {
@@ -46,7 +46,6 @@ int main(int argc, char** argv)
 	start = GetTickCount();
 	GSkyline skyline(filename);
 	//cout << "size:" << skyline.allPoints.size() << endl;
-	cout << "layers:" << skyline.layers.size() << endl;
 	end = GetTickCount();
 	//printf("building cost time: %lld ms\n", end - start);
 	cout << filename << "\t";
@@ -58,30 +57,49 @@ int main(int argc, char** argv)
     end = GetTickCount();
 	//printf("processing cost time: %lld ms\n", end - start);
 	printf("%lld\t", end - start);
+	
+	cout << "layers:" << skyline.layers.size() << "\t" << "size:" << skyline.allPoints.size() << "\t";
+
 	//cout << "preprocessing result:" << endl;
 	long preNum = pre.size();
 	/*for(vector<Group>::iterator it = pre.begin(); it != pre.end(); it++){
 		(*it).Print();
 	}*/
-	cout << "preprocessing end" << endl;
+	//cout << "preprocessing end" << endl;
 
 	Point** all = new Point*[skyline.allPoints.size()];
 	for(int i = 0; i < skyline.allPoints.size(); i++){
 		all[i] = skyline.allPoints[i];
 	}
+	
+	//cout <<"u++2" << endl;
+	start = GetTickCount();
+	long long uwisepp2Num = skyline.UnitWisePlusPlus2(k,all,skyline.allPoints.size());
+	end = GetTickCount();
+	cout <<(uwisepp2Num + preNum) << "\t";
+	cout <<(end - start) << "\t";
 
-
+	
+	//cout <<"u++" << endl;
+	start = GetTickCount();
+	long long uwisepp1Num = skyline.UnitWisePlusPlus1(k,all,skyline.allPoints.size());
+	end = GetTickCount();
+	cout <<(uwisepp1Num + preNum) << "\t";
+	cout <<(end - start) << "\t";
+	
+	
+	//cout << "p wise:" << endl;
 	/*
-	cout << "p wise:" << endl;
 	start = GetTickCount();
 	skyline.PointWisePlus(k);
 	end = GetTickCount();
 	printf("%d\t", skyline.PointWiseCount + preNum);
 	cout <<(end - start) << "\t";
+	*/
 	//for(vector<Group>::iterator it = gs.begin(); it != gs.end(); it++){
 	//(*it).Print();
 	//}
-	
+	/*
 	//cout << "u wise plus:" << endl;
 	start = GetTickCount();
 	int uwiseNum = skyline.UnitWisePlus(k,false);
@@ -91,7 +109,9 @@ int main(int argc, char** argv)
 	//for(vector<UGroup>::iterator it = gs1.begin(); it != gs1.end(); it++){
 	//	(*it).PrintAsc();
 	//}
+	*/	
 
+	/*
 	start = GetTickCount();
 	int uwiseppNum = 0;
 	uwiseppNum = skyline.UnitWisePlusPlus(k);
@@ -99,22 +119,6 @@ int main(int argc, char** argv)
 	cout <<(uwiseppNum + preNum) << "\t";
 	cout <<(end - start) << "\t";
 	*/
-
-
-	cout <<"u++" << endl;
-	start = GetTickCount();
-	long long uwisepp1Num = skyline.UnitWisePlusPlus1(k,all,skyline.allPoints.size());
-	end = GetTickCount();
-	cout <<(uwisepp1Num + preNum) << "\t";
-	cout <<(end - start) << "\t";
-	
-	cout <<"u++2" << endl;
-	start = GetTickCount();
-	long long uwisepp2Num = skyline.UnitWisePlusPlus2(k,all,skyline.allPoints.size());
-	end = GetTickCount();
-	cout <<(uwisepp2Num + preNum) << "\t";
-	cout <<(end - start) << "\t";
-	
 	cout <<"\n";
 
 	
